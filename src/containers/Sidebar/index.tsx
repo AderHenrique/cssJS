6c3d82b452d0199039a83f9427acd7708aa300de
AderHenrique <adercontas@gmail.com>
2023-08-17 18:10:20 -0300

    commit inicial

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/AderHenrique.png" />
+    <Titulo fontSize={20}>Ader Henrique</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,16 @@
+import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Paragrafo tipo="secundario">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta
+      repudiandae fugiat quos temporibus minima. Quisquam, alias. Tempore
+      dignissimos dicta iure, cum sapiente laborum, enim soluta reiciendis
+      magnam ex veritatis cumque.
+    </Paragrafo>
+  </section>
+)
+
+export default Sobre
