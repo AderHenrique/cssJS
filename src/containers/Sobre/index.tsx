49ad68885011056b3464b93ecedb74b9262f3d7f
AderHenrique <adercontas@gmail.com>
2023-08-18 15:08:00 -0300

    termino da secao sobre

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,5 +1,6 @@
 import Titulo from '../../components/Titulo'
 import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
 
 const Sobre = () => (
   <section>
@@ -10,6 +11,10 @@ const Sobre = () => (
       dignissimos dicta iure, cum sapiente laborum, enim soluta reiciendis
       magnam ex veritatis cumque.
     </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=AderHenrique&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=AderHenrique&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
   </section>
 )
 
