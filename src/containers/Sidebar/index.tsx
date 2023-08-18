d8c670f48c41b4b33cf51680d12f99968a30f1dd
AderHenrique <adercontas@gmail.com>
2023-08-18 18:55:13 -0300

    commit final

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,26 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
 
-const Sidebar = () => (
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
+
+type Props = {
+  trocaTema: () => void
+}
+
+const Sidebar = (props: Props) => (
   <aside>
-    <img src="https://github.com/AderHenrique.png" />
-    <Titulo fontSize={20}>Ader Henrique</Titulo>
+    <SidebarContainer>
+      <Avatar></Avatar>
+      <Titulo fontSize={20}>Ader Henrique</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        AderHenrique
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Fugindo do comum...
+      </Descricao>
+      <BotaoTema onClick={props.trocaTema}>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
---
+++ src/themes/light.ts
@@ -0,0 +1,9 @@
+const TemaLight = {
+  corPrincipal: '#282a45',
+  corSecundaria: '#949494',
+  corDeFundo: '#fff',
+  corDeFundoBotao: '#4476bf',
+  corDaBorda: '#c1c1c1'
+}
+
+export default TemaLight
